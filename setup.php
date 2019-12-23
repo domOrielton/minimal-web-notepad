f4a4ae69c88adb19efdd551edd052b4cf54b2a07
domOrielton <dom@pembrokeshire.com>
2019-12-23 11:57:55 +0000

    Added section to check .htaccess
    Added check for .htaccess file to confirm it is there at the code folder level (not the notes file)

--- setup.php
+++ setup.php
@@ -14,10 +14,23 @@ if (makeDir($data_directory)) {
 	// make sure there is an .htaccess file there
 	// if there is not then create one
 	$file = $data_directory .'/.htaccess';
-	if (makehtaccess($file)) { echo '.htaccess file ok<br>'; } else { echo '.htaccess file exists<br>'; } 
-	
+	if (makehtaccess($file)) { echo '.htaccess file ok in the ' . $data_directory. ' data directory<br>'; } else { echo '.htaccess file exists in the ' . $data_directory .' data directory<br>'; } 	
 } else {
 	echo $data_directory . ' folder does not exist and could not be created<br>';
+}
+
+// based on https://raw.githubusercontent.com/bolt/htaccess_tester/master/htaccess_tester.php
+if (is_readable(__DIR__.'/.htaccess') ) {
+
+    echo "<p>The .htaccess file exists in this folder and is readable to the webserver. These are its contents which should match the repos version: </p>\n<pre>";
+    echo file_get_contents(__DIR__.'/.htaccess');
+    echo "</pre>";
+
+}  else {
+
+    echo "<p><strong>Error:</strong> The .htaccess file does not exist in this folder or it is not readable to the webserver. <br><br>Ensure a .htaccess file is here and matches the repos version. Make sure it is readable to the webserver.</p>";
+    die();
+
 }
 
 // https://stackoverflow.com/questions/109188/how-do-i-check-if-a-directory-is-writeable-in-php
